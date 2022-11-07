8e321e6b1aa3227ca8cedf057dc4136ca4c6ba18
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-07 13:46:47 +0500

    e2e tests finished with review

--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -4,6 +4,7 @@ import * as request from 'supertest'
 import { CreateReviewDto } from '../src/review/dto/create-review.dto'
 import { Types, disconnect } from 'mongoose'
 import { AppModule } from '../src/app.module'
+import { REVIEW_NOT_FOUND } from '../src/review/review.constants'
 
 const productId = new Types.ObjectId().toHexString()
 
@@ -17,6 +18,7 @@ const createDto: CreateReviewDto = {
 
 describe('Create and delete review (e2e)', () => {
   let app: INestApplication
+  let createdReviewId: string
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -32,7 +34,38 @@ describe('Create and delete review (e2e)', () => {
       .post('/review/create')
       .send(createDto)
       .expect(201)
+    createdReviewId = body._id
     expect(body._id).toBeDefined()
+  })
+
+  it('should find reviews by productId', async () => {
+    const { body } = await request(app.getHttpServer())
+      .get(`/review/byProduct/${productId}`)
+      .expect(200)
+    expect(body.length).toBe(1)
+  })
+
+  it('should not return reviews if productId does not exist', async () => {
+    const { body } = await request(app.getHttpServer())
+      .get(`/review/byProduct/${new Types.ObjectId().toHexString()}`)
+      .expect(200)
+    expect(body.length).toBe(0)
+  })
+
+  it('should delete review by id', async () => {
+    const { body } = await request(app.getHttpServer())
+      .delete(`/review/${createdReviewId}`)
+      .expect(200)
+  })
+
+  it('should not delete review does not exist', async () => {
+    const { body } = await request(app.getHttpServer())
+      .delete(`/review/${new Types.ObjectId().toHexString()}`)
+      .expect(404, {
+        statusCode: 404,
+        message: REVIEW_NOT_FOUND
+      })
+    console.log(body)
   })
 
   afterAll(() => {
