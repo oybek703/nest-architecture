227d8ad42c9816c968fd890679f827fd995f32ac
Yoriqulov Oybek <hhoybek@gmail.com>
2022-11-13 01:16:02 +0500

    jwt guard added

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,12 +7,14 @@ import {
   HttpStatus,
   Param,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe
 } from '@nestjs/common'
 import { CreateReviewDto } from './dto/create-review.dto'
 import { ReviewService } from './review.service'
 import { REVIEW_NOT_FOUND } from './review.constants'
+import { JwtGuard } from '../auth/guards/jwt.guard'
 
 @Controller('review')
 export class ReviewController {
@@ -33,6 +35,7 @@ export class ReviewController {
     return deletedReview
   }
 
+  @UseGuards(JwtGuard)
   @Get('byProduct/:productId')
   async get(@Param('productId') productId: string) {
     return await this.reviewService.findByProductId(productId)
